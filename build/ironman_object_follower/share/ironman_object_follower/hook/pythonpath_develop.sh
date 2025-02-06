2b73d8587558e26cf89de07da3eb1c566f86746f
marilynbraojos <marilynbraojos@gmail.com>
2025-02-05 20:57:58 -0500

    trying to clear queue

---
+++ build/ironman_object_follower/share/ironman_object_follower/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/burger/ros2_ws/src/ironman_object_follower/build/ironman_object_follower"
