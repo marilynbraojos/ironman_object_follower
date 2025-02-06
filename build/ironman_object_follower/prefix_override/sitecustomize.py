3d9a6b049f9f0b818a5c6fb7de00e633a6237520
michaelangelo <marilynbraojos@gmail.com>
2025-02-05 21:15:27 -0500

    trying stuff

--- build/ironman_object_follower/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/burger/ros2_ws/src/ironman_object_follower/install/ironman_object_follower'
--- build/ironman_object_follower/share/ironman_object_follower/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/burger/ros2_ws/src/ironman_object_follower/build/ironman_object_follower"
